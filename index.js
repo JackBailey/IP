efad83f33d5037007ccc75e071cdb0dec2389247
Jack Bailey <jack@jackbailey.dev>
2023-04-19 12:51:35 +0200

    Add headers option

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ app.get("/", (req, res) => {
   return res.status(200).send(req.ip);
 })
 
+app.get("/headers", (req, res) => {
+    let headerOutput = [];
+    for (const [key, value] of Object.entries(req.headers)) {
+        headerOutput.push(`<b>${key}:</b> ${value}`)
+    }
+
+    headerOutput.push(`<hr><b>IP:</b> ${req.ip}`)
+    return res.status(200).send(headerOutput.join("<br>"));
+})
+
 app.listen(3000, () => {
     console.log("Listening on port 3000!")
 });
